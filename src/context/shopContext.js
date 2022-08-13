0a305dabfa0cd8f08d9deae5a6ca62dc87292342
Santiago Enciso <santiago.enciso@extend.com>
2022-08-13 13:57:39 -0500

    Began building functions that fetch products and checkout.

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -1,67 +1,88 @@
-import React, { Component } from 'react'
-import Client from 'shopify-buy';
+import React, { Component } from "react";
+import Client from "shopify-buy";
 
 const ShopContext = React.createContext();
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
     domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
-    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API
+    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_API,
 });
 
 export class ShopProvider extends Component {
-
     state = {
-        product : {},
+        product: {},
         products: [],
         checkout: {},
         isCartOpen: false,
-        isMenuOpen: false
+        isMenuOpen: false,
+    };
+
+    // Get the checkout when the app loads
+    componentDidMount() {
+        // get checkout
+        if (localStorage.checkout_id) {
+            this.fetchCheckout(localStorage.checkout_id);
+        }
+        else {
+            this.createCheckout();
+        }
     }
-    
+
     createCheckout = async () => {
+        // Create an empty checkout
+        const checkout = await client.checkout.create();
+        // Store checkout ID in local storage
+        localStorage.setItem("checkout_id", checkout.id);
+        // Update checkout state
+        this.setState({ checkout: checkout });
+    };
 
-    }
+    fetchCheckout = async (checkoutId) => {
+        // Fetches checkout
+        client.checkout
+            .fetch(checkoutId)
+            .then((checkout) => {
+                // Saves checkout in state
+                this.setState({ checkout: checkout })
+            });
+    };
 
-    fetchCheckout = async () => {
+    addItemtoCheckout = async () => { };
 
-    }
-
-    addItemtoCheckout = async () => {
-        
-    }
-
-    removeLineItem = async (lineItemIdsToRemove) => {
-
-    }
+    removeLineItem = async (lineItemIdsToRemove) => { };
 
     fetchAllProducts = async () => {
-
-    }
+        // Fetch all products in your shop
+        const products = await client.product.fetchAll();
+        // Update products state
+        this.setState({ products: products });
+    };
 
     fetchProductWithHandle = async (handle) => {
+        // Fetch a single product by Handle
+        const product = await client.product.fetchByHandle(handle);
+        // Update product state
+        this.setState({ product: product });
+    };
 
-    }
+    closeCart = () => { };
 
-    closeCart = () => {}
+    openCart = () => { };
 
-    opentCart = () => {}
-    
-    closeMenu = () => {}
-    
-    openMenu = () => {}
+    closeMenu = () => { };
+
+    openMenu = () => { };
 
     render() {
-    return (
-        <ShopContext.Provider>
-            {this.props.children}
-        </ShopContext.Provider>
-    )
+        console.log(this.state.checkout);
+
+        return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
     }
 }
 
-const ShoConsumer = ShopContext.Consumer
+const ShopConsumer = ShopContext.Consumer;
 
-export {ShoConsumer, ShopContext}
+export { ShopConsumer, ShopContext };
 
-export default ShopProvider
+export default ShopProvider;
