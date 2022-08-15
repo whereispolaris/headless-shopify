41f231c52e862cd01fa93c8e289d83192e37eb54
Santiago Enciso <santiago.enciso@extend.com>
2022-08-14 19:02:25 -0500

    Added React Router components, created Routes for Home and ProductPage components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <p>Navigation</p>
+        <Switch>
+          <Route path="/products/:handle">
+            <ProductPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
