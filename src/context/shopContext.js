9b5cb2e1b5f492329465a1b4e5ea4eb002a05651
Santiago Enciso <santiago.enciso@extend.com>
2022-08-28 17:01:23 -0500

    Added functionality to closeCart and openCart functions.

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -66,9 +66,9 @@ export class ShopProvider extends Component {
         this.setState({ product: product });
     };
 
-    closeCart = () => { };
+    closeCart = () => { this.setState({ isCartOpen: false }) };
 
-    openCart = () => { };
+    openCart = () => { this.setState({ isCartOpen: true }) };
 
     closeMenu = () => { };
 
