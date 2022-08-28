786b806fbece91fa5d3fec91fcf499ff3c63b248
Santiago Enciso <santiago.enciso@extend.com>
2022-08-28 14:33:24 -0500

    Imported Chakra into project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopProvider from "./context/shopContext";
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ShopProvider>
-    <App />
-  </ShopProvider>
+  <ChakraProvider>
+    <ShopProvider>
+      <App />
+    </ShopProvider>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
