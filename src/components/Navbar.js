7699b03db52ff9975675dc5dd130e4f4d0a4c85e
Santiago Enciso <santiago.enciso@extend.com>
2022-08-28 17:02:21 -0500

    Added onClick function to cart icon.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,9 @@ const Navbar = () => {
         <Flex backgroundColor="#FFA8E2" flexDir="row" justifyContent="space-between" p="2rem">
             <Icon fill="white" cursor="pointer" as={MdMenu} w={30} h={30} />
             <Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg" h={100} w={100} />
-            <Icon fill="white" cursor="pointer" as={MdShoppingBasket} w={30} h={30} />
+            <Icon fill="white" cursor="pointer" as={MdShoppingBasket} w={30} h={30}
+                onClick={() => openCart()}
+            />
         </Flex>
     )
 }
