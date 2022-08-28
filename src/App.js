d2db066f5591f831b963c4cad7186f33d75a657f
Santiago Enciso <santiago.enciso@extend.com>
2022-08-28 17:01:45 -0500

    Imported Cart component to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import ProductPage from "./pages/ProductPage";
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Cart />
         <Switch>
           <Route path="/products/:handle">
             <ProductPage />
