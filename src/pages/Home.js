fc4f4b7e14782c5218e97a3ca6587850298d0606
Santiago Enciso <santiago.enciso@extend.com>
2022-08-28 14:35:23 -0500

    Imported Chakra components to Home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
 
 const Home = () => {
 
@@ -12,17 +13,29 @@ const Home = () => {
         fetchAllProducts();
     }, [fetchAllProducts])
 
+    console.log("products: ", products);
+
     // Return loading message if no products are availables
     if (!products) return <div>Loading...</div>
 
     return (
-        <div>
-            {
-                products.map(product => (
-                    <Link to={`/products/${product.handle}`} key={product.title}>{product.title}</Link>
-                ))
-            }
-        </div>
+        <Box>
+            <Grid templateColumns="repeat(3, 1fr)">
+                {
+                    products.map(product => (
+                        <Link
+                            to={`/products/${product.handle}`}
+                            key={product.id}>
+                            <Box _hover={{ opacity: '80%' }} textAlign="center">
+                                <Image src={product.images[0].src} />
+                                <Text>{product.title}</Text>
+                                <Text>{product.variants[0].price}</Text>
+                            </Box>
+                        </Link>
+                    ))
+                }
+            </Grid>
+        </Box>
     )
 }
 
