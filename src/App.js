0e8cf5b54a42bc616ff090962c5bbf6e36ecc260
Santiago Enciso <whereispolaris@gmail.com>
2022-10-31 10:49:51 -0500

    Imported NavBar and NavMenu into App component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import ProductPage from "./pages/ProductPage";
 import Cart from "./components/Cart";
+import NavMenu from "./components/NavMenu";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <NavBar />
         <Cart />
+        <NavMenu />
         <Switch>
           <Route path="/products/:handle">
             <ProductPage />
