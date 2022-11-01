5ad83061ac15190783640cf9865b7cf8cb366232
Santiago Enciso <whereispolaris@gmail.com>
2022-11-01 09:53:27 -0500

    Added more styling to ProductPage and imported ExtendOffers component.

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useLayoutEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
 import { Box, Grid, Image, Text, Button, Heading, Flex, Center } from '@chakra-ui/react'
+import ExtendOffers from "../components/ExtendOffers";
 
 const ProductPage = () => {
 
@@ -20,14 +21,19 @@ const ProductPage = () => {
     if (!product.title) return <div>Loading...</div>
 
     return (
-        <Box>
-            <Grid templateColumns="repeat(2, 1fr)">
-                <Image src={product.images[0].src} />
+        <Box p="2rem">
+            <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} m="auto">
+                <Flex justifyContent="center" alignItems="center">
+                    <Image src={product.images[0].src} />
+                </Flex>
                 <Box>
-                    <Heading>{product.title}</Heading>
+                    <Heading pd="2rem">{product.title}</Heading>
                     <Text>{product.variants[0].price}</Text>
                     <Text>{product.description}</Text>
+                    {/*Extend - Renders offers on product page*/}
+                    <ExtendOffers/>
                     <Button
+                        className="add-to-cart"
                         onClick={() => addItemToCheckout(product.variants[0].id, 1)}
                     >Add To Cart</Button>
                 </Box>
