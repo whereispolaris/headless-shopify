03039153921b398443937f75880ac9ea5a4c4853
Santiago Enciso <whereispolaris@gmail.com>
2022-11-02 16:41:56 -0500

    Updated styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,18 @@ import {Badge, Box, Flex, Icon, Image, Text} from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext'
 import { Link } from 'react-router-dom'
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'
+import headless from '../img/headless.png'
 
 const NavBar = () => {
 
     const { openCart, openMenu, checkout } = useContext(ShopContext);
 
     return (
-        <Flex backgroundColor="#FFABE2" flexDir="row" alignItems="center" justifyContent="space-between" p="2rem">
+        <Flex backgroundColor="#090637" flexDir="row" alignItems="center" justifyContent="space-between" p="1rem">
             <Icon fill="white" as={MdMenu} w="30" h="30"
                   onClick={() => openMenu()}
             />
-            <Link to="/"><Image src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Logologo_1.svg" w="100" h="100"/></Link>
+            <Link to="/"><Image src={headless} w="100" h="100"/></Link>
             <Box>
                 <Icon fill="white" as={MdShoppingBasket} w="30" h="30"
                       onClick={() => openCart()}
